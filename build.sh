74be1359662d2d2202ace609b82b21b06e95c624
Marius André Elsfjordstrand Beck <marius.beck@nb.no>
2018-10-18 12:55:17 +0200

    chore: give generated files a+rwx permissions (generated in docker container running as root)

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env sh
 
+# generate go language proto files using official grpc/go docker image
 docker run -v "$(pwd)":/go/src/github.com/nlnwa/maalfrid-api/ grpc/go:1.0 \
-/bin/bash -c "go generate github.com/nlnwa/maalfrid-api"
+/bin/bash -c \
+"go generate github.com/nlnwa/maalfrid-api && chmod -R 777 src/github.com/nlnwa/maalfrid-api/gen"
