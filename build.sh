c3e340099fd70f67ceea0c0e3f512c0ea5ab1fb1
Marius André Elsfjordstrand Beck <marius.beck@nb.no>
2018-10-22 14:52:41 +0200

    feat: add .travis.yml

--- build.sh
+++ build.sh
@@ -2,5 +2,4 @@
 
 # generate go language proto files using official grpc/go docker image
 docker run -v "$(pwd)":/go/src/github.com/nlnwa/maalfrid-api/ grpc/go:1.0 \
-/bin/bash -c \
-"go generate github.com/nlnwa/maalfrid-api && chmod -R 777 src/github.com/nlnwa/maalfrid-api/gen"
+/bin/bash -c "go generate github.com/nlnwa/maalfrid-api"
