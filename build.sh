6e37c76db0aba35fbf2db230aa01165d8429fa4f
Marius André Elsfjordstrand Beck <marius.beck@nb.no>
2019-07-16 16:17:03 +0200

    Make build work with podman and docker

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env sh
 
+SECURITY_OPT=""
+if command -v podman > /dev/null 2>&1; then
+    SECURITY_OPT="--security-opt label=disable"
+fi
+
 # generate go language proto files using official grpc/go docker image
-docker run -v "$(pwd)":/go/src/github.com/nlnwa/maalfrid-api/ grpc/go:1.0 \
+docker run ${SECURITY_OPT} --rm -v "$(pwd)":/go/src/github.com/nlnwa/maalfrid-api/ grpc/go:1.0 \
 /bin/bash -c "go generate github.com/nlnwa/maalfrid-api"
