045b7c053d06596fac75acadf95f611fd4bb53d4
Marius André Elsfjordstrand Beck <marius.beck@nb.no>
2019-10-02 09:58:14 +0200

    Support podman when running build script

--- build.sh
+++ build.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env sh
 
+COMMAND=docker
 SECURITY_OPT=""
 if command -v podman > /dev/null 2>&1; then
     SECURITY_OPT="--security-opt label=disable"
+    COMMAND=podman
 fi
 
 # generate go language proto files using official grpc/go docker image
-docker run ${SECURITY_OPT} --rm -v "$(pwd)":/go/src/github.com/nlnwa/maalfrid-api/ grpc/go:1.0 \
+${COMMAND} run ${SECURITY_OPT} --rm -v "$(pwd)":/go/src/github.com/nlnwa/maalfrid-api/ grpc/go:1.0 \
 /bin/bash -c "go generate github.com/nlnwa/maalfrid-api"
